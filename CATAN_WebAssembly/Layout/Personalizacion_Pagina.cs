8f740d04833bc11823aa93c9802c9c86ff7fd408
Santiago Affre <santiagoaffre.123@gmail.com>
2026-07-19 23:47:00 -0300

    Version 2.1 se ajusto el overlay (vertices y aristas) exitosamente

--- CATAN_WebAssembly/Layout/Personalizacion_Pagina.cs
+++ CATAN_WebAssembly/Layout/Personalizacion_Pagina.cs
@@ -2,6 +2,7 @@
 {
     public class Personalizacion_Pagina
     {
+
         private double valor_gapratio = 0.15;
 
         /// <summary>
@@ -9,13 +10,13 @@
         /// </summary>
         public double Valor_Gapratio
         {
-            get { return (valor_gapratio ); } //se le agrega 1, porque el slider va de -1 a 0
+            get { return valor_gapratio; } //se le agrega 1, porque el slider va de -1 a 0
             set
             {
                 if (valor_gapratio != value) //si no cambia el valor, no invoca el metodo de cambio
                 {
-                    valor_gapratio = value;
-                    NotifyStateChanged(); // avisa que algo cambió
+                   valor_gapratio = value;
+                   NotifyStateChanged(); // avisa que algo cambió
                 }
             }
         }
@@ -49,6 +50,50 @@
                 if (dark_mode != value) //si no cambia el valor, no invoca el metodo de cambio
                 {
                     dark_mode = value;
+                    NotifyStateChanged(); // avisa que algo cambió
+                }
+            }
+        }
+
+        private double offsetX = 0;
+
+        public double OffsetX
+        {
+            get { return offsetX; }
+            set
+            {
+                if (offsetX != value) //si no cambia el valor, no invoca el metodo de cambio
+                {
+                    offsetX = value;
+                    NotifyStateChanged(); // avisa que algo cambió
+                }
+            }
+        }
+
+        private double offsetY = 0;
+
+        public double OffsetY
+        {
+            get { return offsetY; }
+            set
+            {
+                if (offsetY != value) //si no cambia el valor, no invoca el metodo de cambio
+                {
+                    offsetY = value;
+                    NotifyStateChanged(); // avisa que algo cambió
+                }
+            }
+        }
+        private double overlayScale = 0.9;
+
+        public double OverlayScale
+        {
+            get { return overlayScale; }
+            set
+            {
+                if (overlayScale != value) //si no cambia el valor, no invoca el metodo de cambio
+                {
+                    overlayScale = value;
                     NotifyStateChanged(); // avisa que algo cambió
                 }
             }
